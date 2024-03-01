a338a88c6711ddc0b209ea86bf977fdbf273f60f
coderproa <161629778+coderproa@users.noreply.github.com>
2024-03-01 10:11:20 -0300

    Create while.py

---
+++ while.py
@@ -0,0 +1,8 @@
+## Crear un programa que muestre por pantalla la palabra hola diez veces utilizando el ciclo while.
+
+contador = 0
+
+while contador < 10:
+  print("Hola")
+  contador = contador + 1
+
