be5d79b5603ca2d5c76dd799a64f9077f71be034
coderproa <161629778+coderproa@users.noreply.github.com>
2024-03-04 09:51:05 -0300

    Create poo.py

---
+++ poo.py
@@ -0,0 +1,4 @@
+## Crear una clase llamada Vehiculo capaz de procesar la informacion basica de los autos en una consencionaria. Debera tener constructor y los atributos del vehiculo seran: 
+## patente, marca, modelo, kilometraje. Crear metodos (de acceso) getter y (modificacion) setter. Mostrar por pantalla al menos un atributo y modificar el kilometraje.
+
+
